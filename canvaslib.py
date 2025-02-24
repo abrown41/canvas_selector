2cfd08e2a814b2f65cd724cef211d694a9e82677
Andrew Brown <andrew.brown@qub.ac.uk>
2025-02-24 17:06:39 +0000

    remove argument parsing (for library)

--- canvaslib.py
+++ canvaslib.py
@@ -1,10 +1,42 @@
 from canvasapi import Canvas
 from APIKEY import API_KEY, API_URL
-from test_exercises import studentTest
 from tqdm import tqdm
+from argparse import Namespace
 
 canvas = Canvas(API_URL, API_KEY)
 
+
+class options(Namespace):
+
+    def __init__(self,
+                 semester=None,
+                 course=None,
+                 assignment=None,
+                 groups=None):
+        super().__init__()
+        self.semester = semester
+        self.course = course
+        self.assignment = assignment
+        self.groups = groups
+
+
+def choose_options(args):
+    if not args.course:
+        args.course = select_course(canvas.get_courses(state='available'),
+                                    enrollment_term_id=args.semester)
+    else:
+        args.course = canvas.get_course(args.course)
+
+    if args.groups:
+        args.groupid = choose_one(args.course.get_group_categories())
+
+    if args.assignment:
+        asses = args.course.get_assignments(include="assignment")
+        asses = choose_many(asses, obj="assignment")
+        args.asses = [args.course.get_assignment(x)
+                      for x in asses]
+
+    return args
 
 def select_course(courseList, enrollment_term_id=None):
 
@@ -17,36 +49,6 @@ def select_course(courseList, enrollment_term_id=None):
     answers = choose_one(courses, obj="course")
 
     return answers
-
-
-def read_command_line():
-    from argparse import ArgumentParser as AP
-    parser = AP()
-    parser.add_argument('-c', '--course', help="Canvas courseID",
-                        default=None)
-    parser.add_argument('-a', '--assignment', help='assignment id',
-                        default=None)
-    parser.add_argument('--all', help='get all assignments',
-                        action='store_true', default=False)
-
-    args = parser.parse_args()
-    if not args.course:
-        args.course = choose_course(canvas)
-    else:
-        args.course = canvas.get_course(args.course)
-
-    asses = args.course.get_assignments(include="assignment")
-    if args.all:
-        args.asses = asses
-    else:
-        if not args.assignment:
-            args.assignment = choose_many(asses, obj="assignment")
-            args.asses = [args.course.get_assignment(x)
-                          for x in args.assignment]
-        else:
-            args.asses = [args.course.get_assignment(args.assignment)]
-
-    return args
 
 
 def choose_many(pagList, obj="course"):
@@ -75,10 +77,6 @@ def choose_one(pagList, obj="course"):
     return answers[obj]
 
 
-def choose_course(canvas):
-    return choose_one(canvas.get_courses(state='available'))
-
-
 def mkdir(ass_id):
     import os
     if os.path.isdir(str(ass_id)):
@@ -93,46 +91,21 @@ def nameFile(sub):
     return f"{sub.assignment_id}/u{sub.user_id}_{fname}"
 
 
-def download_ungraded(sub):
+def download_submission(sub, suffix=None):
 
     if len(sub.attachments) > 0:
         mkdir(sub.assignment_id)
         thefile = sub.attachments[-1]
         downname = nameFile(sub)
-        if downname.endswith(".ipynb"):
-            thefile.download(downname)
-
-
-def mark_ungraded(sub, ass):
-    if len(sub.attachments) > 0:
-        downname = nameFile(sub)
-        mark = int(ass.points_possible) * studentTest(downname)
-        sub.mark_read()
-        if sub.grade:
-            if mark > int(sub.grade):
-                update_grade(sub, mark)
+        if suffix:
+            if downname.endswith(suffix):
+                thefile.download(downname)
         else:
-            update_grade(sub, mark)
-
-
-def clear_testsrc():
-    """ check if testsrc is installed, and if so, use pip to
-    uninstall it"""
-    from importlib.util import find_spec
-    installed = find_spec('testsrc') is not None
-    if installed:
-        import subprocess
-        import sys
-        subprocess.check_call(
-            [sys.executable, "-m", "pip", "uninstall", "-y", "testsrc"],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.STDOUT)
-    return
+            thefile.download(downname)
 
 
 def cleanup(ass):
     import shutil
-    clear_testsrc()
     shutil.rmtree(str(ass.id))
 
 
@@ -143,9 +116,7 @@ def get_submissions(ass):
     ungraded = [sub for sub in subs if len(sub.attachments) > 0 and
                 (sub.grade == "0" or sub.grade is None)]
     for sub in tqdm(ungraded, desc=f"Downloading {ass.name}", ascii=True):
-        download_ungraded(sub)
-    for sub in tqdm(ungraded, desc=f"Grading     {ass.name}", ascii=True):
-        mark_ungraded(sub, ass)
+        download_submission(sub)
 
 
 def update_grade(sub, newgrade):
@@ -153,8 +124,6 @@ def update_grade(sub, newgrade):
 
 
 if __name__ == '__main__':
-    args = read_command_line()
-
-    for ass in args.asses:
-        ass.edit(assignment={'post_manually': False})
-        print(ass.post_manually)
+    #  args = read_command_line()
+    args = options(semester='2241_SPR', groups=True)
+    choose_options(args)
